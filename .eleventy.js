90236760f77277da4430fe2ca7fbc5ef594c66cf
Tyler Mercer <acct@tylermercer.net>
2023-01-11 20:00:46 -0700

    Remove unused passthrough for CSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addWatchTarget("./src/sass/");
 
-    eleventyConfig.addPassthroughCopy("./src/style.css");
     return {
       dir: {
         input: "src",
