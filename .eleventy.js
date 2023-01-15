bac09836fc6300a1c40a5e8b1ca4daae63d89127
Tyler Mercer <acct@tylermercer.net>
2023-01-14 20:38:41 -0700

    Apply collection layout to collection root pages

--- .eleventy.js
+++ .eleventy.js
@@ -3,8 +3,7 @@ const { DateTime } = require("luxon");
 module.exports = function (eleventyConfig) {
     eleventyConfig.addLayoutAlias('base', 'layouts/base.njk');
     eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
-    eleventyConfig.addLayoutAlias('post-faith', 'layouts/post-faith.njk');
-    eleventyConfig.addLayoutAlias('post-software', 'layouts/post-software.njk');
+    eleventyConfig.addLayoutAlias('page', 'layouts/page.njk');
 
     eleventyConfig.addFilter("postDate", (dateObj) => {
       return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
