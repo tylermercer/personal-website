af894ca9a4bd662f57d03bd29a7b0834b4e954f9
Tyler Mercer <acct@tylermercer.net>
2023-01-14 18:43:41 -0700

    Apply bg patterns to titles of posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
 const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
-eleventyConfig.addLayoutAlias('base', 'layouts/base.njk');
+    eleventyConfig.addLayoutAlias('base', 'layouts/base.njk');
     eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
+    eleventyConfig.addLayoutAlias('post-faith', 'layouts/post-faith.njk');
+    eleventyConfig.addLayoutAlias('post-software', 'layouts/post-software.njk');
 
     eleventyConfig.addFilter("postDate", (dateObj) => {
       return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
