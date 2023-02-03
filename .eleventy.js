3e4deb07c8ce05253a39d4c2a676dce3749a9182
Tyler Mercer <acct@tylermercer.net>
2023-02-02 21:04:33 -0700

    Load Figtree from fontsource

--- .eleventy.js
+++ .eleventy.js
@@ -39,8 +39,8 @@ module.exports = function (eleventyConfig) {
   
     eleventyConfig.setLibrary("md", markdown);
 
-    eleventyConfig.addPassthroughCopy("./src/fonts/*");
     eleventyConfig.addPassthroughCopy({ "./src/static/*" : "/" });
+    eleventyConfig.addPassthroughCopy({ './node_modules/@fontsource/figtree/files/*latin-{400,700}*.woff2': '/css/files' })
     eleventyConfig.addPassthroughCopy({ './node_modules/@fontsource/faustina/files/*latin-{400,700}*.woff2': '/css/files' })
 
     eleventyConfig.addWatchTarget("./src/sass/");
