e0469ad40f88b87e1d7f68bf6507f512e3aec8f1
Tyler Mercer <acct@tylermercer.net>
2023-02-09 21:33:34 -0700

    Add RSS feed

---
+++ src/_data/metadata.js
@@ -0,0 +1,10 @@
+module.exports = {
+	"title": "Tyler Mercer",
+	"url": "https://tylermercer.net/",
+	"language": "en",
+	"description": "",
+	"author": {
+		"name": "Tyler Mercer",
+		"url": "https://tylermercer.net/about/"
+	}
+}
