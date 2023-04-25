d5beb29e7e0a4e30b141d25a8c4226a8a8f3eeeb
Tyler Mercer <acct@tylermercer.net>
2023-04-24 21:18:27 -0600

    Move structure of homepage to home layout file
    This will make it easier to reuse for the spanish homepage

--- .eleventy.js
+++ .eleventy.js
@@ -28,6 +28,7 @@ function rmDir(dirPath, removeSelf) {
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addLayoutAlias('base', 'layouts/base.njk');
+    eleventyConfig.addLayoutAlias('home', 'layouts/home.njk');
     eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
     eleventyConfig.addLayoutAlias('page', 'layouts/page.njk');
 
