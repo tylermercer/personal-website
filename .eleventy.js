649d73868cf9c0d577dc504a2d9af9406d91e8dc
Tyler Mercer <acct@tylermercer.net>
2023-05-16 20:46:10 -0600

    Move public folder out of src
    Tina (which I am no longer going to add, because it does not support
    footnotes) puts HTML into the public folder, and 11ty tried to turn that
    into pages in the 11ty build because it was inside src
    
    But this is just a good thing to do anyway

--- .eleventy.js
+++ .eleventy.js
@@ -144,7 +144,7 @@ module.exports = function (eleventyConfig) {
   
     eleventyConfig.setLibrary("md", markdown);
 
-    eleventyConfig.addPassthroughCopy({ "./src/_static/*" : "/" });
+    eleventyConfig.addPassthroughCopy({ "./public/*" : "/" });
     eleventyConfig.addPassthroughCopy({ './node_modules/@fontsource/figtree/files/*latin-{400,700}*.woff2': '/css/files' })
     eleventyConfig.addPassthroughCopy({ './node_modules/@fontsource/faustina/files/*latin-{300,400,700}*.woff2': '/css/files' })
 
