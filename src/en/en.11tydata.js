b43a45172b0b0ce2b84338b14aaa635acad4e0f5
Tyler Mercer <acct@tylermercer.net>
2023-07-03 08:09:52 -0600

    Use filters instead of duplicating them in en.11tydata.js

--- src/en/en.11tydata.js
+++ src/en/en.11tydata.js
@@ -1,13 +1,7 @@
-const stripDefaultLocale = (path) => {
-    return path?.startsWith('/en') ? path.substring(3) : path;
-}
-
-const trimTrailingIndex = (path) => {
-    return path?.endsWith('/index') ? path.slice(0, -6) : path;
-}
-
 module.exports = {
     lang:"en",
     dir:"ltr",
-    permalink: data => `${trimTrailingIndex(stripDefaultLocale(data.page.filePathStem))}${data.pagination?.pageNumber > 0 ? '/' + data.pagination.pageNumber : ''}/index.html`
+    permalink: function(data) {
+        return `${this.trimTrailingIndex(this.stripDefaultLocale(data.page.filePathStem))}${data.pagination?.pageNumber > 0 ? '/' + data.pagination.pageNumber : ''}/index.html`;
+    }
 }
