ebd8037b28bd18e454b9607c1bfeb437cfb206df
Tyler Mercer <acct@tylermercer.net>
2023-07-08 17:50:10 -0600

    Abort email send when latest post was not posted today

--- send-email.js
+++ send-email.js
@@ -12,42 +12,48 @@ const segments = {
   'faith': process.env.SENDGRID_SEGMENT_FAITH,
 }
 
-console.log(segments);
-
 // Read the JSON feed
-const feedPath = path.join(__dirname, 'dist', 'feeds', 'feed.json');
-const feedData = JSON.parse(fs.readFileSync(feedPath, 'utf8'));
+const feedData = JSON.parse(fs.readFileSync(path.join(__dirname, 'dist', 'feeds', 'feed.json'), 'utf8'));
 
 const latestPost = feedData.items[0];
 
-const format = (html, url) => {
-  return `<div><p><small><a href="${url}">View this article online</a></small></p>${html}</div>`
-}
+//Check date
+const wasPostedToday = new Date().toISOString().split('T')[0] == latestPost.date_published.split('T')[0];
 
-// Set up the API request
-const request = {
-  method: 'POST',
-  url: '/v3/marketing/singlesends',
-  body: {
-    name: latestPost.title,
-    email_config: {
-      html_content: format(latestPost.content_html, latestPost.url),
-      subject: latestPost.title,
-    },
-    send_to: {
-      segment_ids: [ segments[latestPost.category] ]
-    }
+if (wasPostedToday) {
+  console.log(`${latestPost.title} was posted today! Sending email...`);
+  const format = (html, url) => {
+    return `<div><p><small><a href="${url}">View this article online</a></small></p>${html}</div>`
   }
-};
-
-console.log(request);
-
-// Send the request
-client.request(request)
-  .then(([response, body]) => {
-    console.log('Status Code:', response.statusCode);
-    console.log('Response Body:', response.body);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+  
+  // Set up the API request
+  const request = {
+    method: 'POST',
+    url: '/v3/marketing/singlesends',
+    body: {
+      name: latestPost.title,
+      email_config: {
+        html_content: format(latestPost.content_html, latestPost.url),
+        subject: latestPost.title,
+      },
+      send_to: {
+        segment_ids: [ segments[latestPost.category] ]
+      }
+    }
+  };
+  
+  console.log(request);
+  
+  // Send the request
+  client.request(request)
+    .then(([response, body]) => {
+      console.log('Status Code:', response.statusCode);
+      console.log('Response Body:', response.body);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+else {
+  console.log(`${latestPost.title} was not posted today; skipping email.`);
+}
