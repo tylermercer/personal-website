7323b7a19d4f37e1cb55ea9859b69ef7a34455b8
Tyler Mercer <acct@tylermercer.net>
2023-10-03 20:51:23 -0600

    Start porting post route over
    previousPost, nextPost and the post metadata are not
    yet implemented. And for some reason, posts at a non-root
    route are not matching....

--- astro/src/utils/utils.ts
+++ astro/src/utils/utils.ts
@@ -1,4 +1,4 @@
-import type { CollectionEntry } from "astro:content";
+import { getCollection, type CollectionEntry } from "astro:content";
 import { DateTime } from "luxon";
 
 export function firstFive(collection: any[]) {
@@ -37,4 +37,11 @@ export function filterOutDraftsIfProduction(entries: CollectionEntry<'posts'>[])
     return entries.filter(
         (e: CollectionEntry<'posts'>) => !('draft' in e.data && e.data.draft) && !('date' in e.data && e.data.date > now)
     );
+}
+
+export async function getCategory(entry: CollectionEntry<'posts'>): Promise<CollectionEntry<'categories'> | undefined> {
+    const categories = await getCollection('categories');
+    const category = entry.id.split('/').at(0);
+    if (!category) return undefined;
+    return categories.find(c => c.data.slug === category);
 }
