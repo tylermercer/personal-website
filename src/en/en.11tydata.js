ead0919763b086b252b08cce05bac843ab897cc1
Tyler Mercer <acct@tylermercer.net>
2023-10-05 15:37:51 -0600

    Delete 11ty site
    👋

--- src/en/en.11tydata.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    lang:"en",
-    dir:"ltr",
-    permalink: function(data) {
-        return `${this.trimTrailingIndex(this.stripDefaultLocale(data.page.filePathStem))}${data.pagination?.pageNumber > 0 ? '/' + data.pagination.pageNumber : ''}/index.html`;
-    }
-}
