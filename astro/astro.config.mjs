cf22bb656592269889e93cc03ea9072884211d08
Tyler Mercer <acct@tylermercer.net>
2023-10-04 21:11:16 -0600

    Fix footnotes in Astro
    May need to check existing articles to make sure they aren't broken

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import remarkFootnote from 'remark-footnotes';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://tylermercer.net'
+    site: 'https://tylermercer.net',
+    markdown: {
+        remarkPlugins: [[remarkFootnote, {inlineNotes: true}]],
+    }
 });
