b0c43370ce4796e2862edcad162afffe40f9bdce
Tyler Mercer <acct@tylermercer.net>
2023-10-04 20:53:42 -0600

    Render footnotes in feeds
    But not in main articles because Astro doesn't use markdown-it....

--- astro/src/utils/utils.ts
+++ astro/src/utils/utils.ts
@@ -2,8 +2,17 @@ import { getCollection, type CollectionEntry } from "astro:content";
 import { DateTime } from "luxon";
 import MarkdownIt from 'markdown-it';
 import sanitizeHtml from 'sanitize-html';
+import anchor from 'markdown-it-anchor';
+import footnote from 'markdown-it-footnote';
 
-const parser = new MarkdownIt();
+const parser = new MarkdownIt({
+    html: true,
+    typographer: true
+  }).use(anchor, {
+    permalink: anchor.permalink.headerLink({
+      safariReaderFix: true
+    })
+  }).use(footnote);
 
 export function firstFive(collection: any[]) {
     return collection.slice(0, 5);
