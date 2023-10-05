cda900791bc2ec74979c646489f0e2c59e291354
Tyler Mercer <acct@tylermercer.net>
2023-10-04 20:08:39 -0600

    Add RSS/XML feed

--- astro/src/utils/metadata.ts
+++ astro/src/utils/metadata.ts
@@ -1,4 +1,17 @@
-export default {
-    title: "Tyler Mercer",
-    baseUrl: "https://tylermercer.net"
-}
+export default
+    {
+        title: "Tyler Mercer",
+        baseUrl: "https://tylermercer.net",
+        twitter: "tylermercerdev",
+        language: "en",
+        description: "Web developer, behavioral design enthusiast, and follower of Jesus",
+        author: {
+            name: "Tyler Mercer",
+            url: "https://tylermercer.net/about/"
+        },
+        builtAt: new Date().toLocaleString("en-US", {
+            dateStyle: "full",
+            timeStyle: "long",
+            timeZone: "UTC"
+        }),
+    }
