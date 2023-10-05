b0e5203a98caddbc60ac99116cbd38b39ced66a8
Tyler Mercer <acct@tylermercer.net>
2023-10-05 16:13:54 -0600

    Add next and prev links

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -66,3 +66,17 @@ export function checkCategory(entry: CollectionEntry<'posts'>, knownCategorySlug
 export function renderMarkdown(markdown: string) {
     return sanitizeHtml(parser.render(markdown));
 }
+
+export type WithNextAndPrev<T> = {
+    item: T,
+    next?: T,
+    prev?: T
+}
+
+export function toWithNextAndPrev<T>(items: T[], reverse: boolean = false): WithNextAndPrev<T>[] {
+    return items.map((t, i, a) => ({
+        item: t,
+        next: reverse ? a[i-1] : a[i+1],
+        prev: reverse ? a[i+1] : a[i-1]
+    }));
+}
