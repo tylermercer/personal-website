d12de00473906b498060b165d806f013dcd2db31
Tyler Mercer <acct@tylermercer.net>
2023-10-06 06:28:18 -0600

    Fix email link on contact page
    MDX! 🎉

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import remarkFootnote from 'remark-footnotes';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://tylermercer.net',
-    markdown: {
-        remarkPlugins: [[remarkFootnote, {inlineNotes: true}]],
-    }
+  site: 'https://tylermercer.net',
+  markdown: {
+    remarkPlugins: [[remarkFootnote, {
+      inlineNotes: true
+    }]]
+  },
+  integrations: [mdx()]
 });
