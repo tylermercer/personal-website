f3cf58e6a2409b8c63383dd6ddb56aa5f2360955
Tyler Mercer <acct@tylermercer.net>
2023-10-06 06:45:25 -0600

    Fix and refresh code styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,18 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [[remarkFootnote, {
       inlineNotes: true
-    }]]
+    }]],
+    shikiConfig: {
+        // Choose from Shiki's built-in themes (or add your own)
+        // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+        theme: 'material-theme-ocean',
+        // Add custom languages
+        // Note: Shiki has countless langs built-in, including .astro!
+        // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+        langs: [],
+        // Enable word wrap to prevent horizontal scrolling
+        wrap: true,
+    }
   },
   integrations: [mdx()]
 });
