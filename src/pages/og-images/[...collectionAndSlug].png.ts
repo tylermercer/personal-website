da4b7440f38976786d6f70161a2cb13d76da4bab
Tyler Mercer <acct@tylermercer.net>
2023-10-07 16:57:15 -0600

    Render HTML in OG image endpoint
    To be rendered using Satori

--- src/pages/og-images/[...collectionAndSlug].png.ts
+++ src/pages/og-images/[...collectionAndSlug].png.ts
@@ -1,6 +1,5 @@
 import { getCollection, getEntryBySlug, type ContentCollectionKey, type CollectionEntry } from "astro:content";
 import type { APIRoute } from "astro";
-import { html } from "satori-html";
 import styles from './_styles.css?raw';
 
 import { labelDrafts, sortByDate, filterOutDraftsIfProduction, getCategory, formatPostDate } from "../../utils/utils";
@@ -16,18 +15,36 @@ export async function getStaticPaths() {
         .concat(pages.map(toParams('pages')));
 }
 
+const renderHtml = (content: CollectionEntry<'posts'> | CollectionEntry<'pages'>, category?: CollectionEntry<'categories'>) => {
+    
+    const { title, description } = content.data;
+    const date: Date | undefined = ('date' in content.data) ? content.data.date : undefined;
+
+    return `
+        <style>
+            ${styles}
+        </style>
+        <div class="root">
+            <div class="pattern pattern-${category?.id || ''}">
+                <h1 class="title">${title}</h1>
+            </div>
+            <div class="bg-container">
+                <div class="description">${description}</div>
+                ${date ? `<div class="dateline">${formatPostDate(date)} — Tyler Mercer</div>` : ''}
+            </div>
+        </div>
+        `.trim();
+}
+
 export const GET: APIRoute = async ({ params }) => {
     const [ collectionRaw, slug ] = params.collectionAndSlug!.split(/\/(.*)/s);
     const collection = collectionRaw as ContentCollectionKey;
 
     const entry = await getEntryBySlug(collection, slug!!) as CollectionEntry<'posts'> | CollectionEntry<'pages'>;
+    const category = (collection === 'posts') ? await getCategory(entry as CollectionEntry<'posts'>) : undefined;
     
     return new Response(
-        //TODO: generate png using slug
-        JSON.stringify({
-            slug,
-            collection,
-            title: entry.data.title
-        })
+        //TODO: generate png using data
+        renderHtml(entry, category)
     );
 }
