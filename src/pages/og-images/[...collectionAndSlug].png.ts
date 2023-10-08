63de8250e0737271b389c4b2f0aacd987c386eb2
Tyler Mercer <acct@tylermercer.net>
2023-10-07 18:07:41 -0600

    Render SVG as image using Sharp
    For some reason, the background pattern does not render properly

--- src/pages/og-images/[...collectionAndSlug].png.ts
+++ src/pages/og-images/[...collectionAndSlug].png.ts
@@ -1,11 +1,12 @@
 import { getCollection, getEntryBySlug, type ContentCollectionKey, type CollectionEntry } from "astro:content";
 import type { APIRoute } from "astro";
-import { html } from "satori-html";
+import { html as htmlToSatori } from "satori-html";
 import satori from "satori";
+import sharp from "sharp";
 
 import styles from './_styles.css?raw';
-import faustina from '@fontsource/faustina/files/faustina-latin-ext-400-normal.woff';
-import figtree from '@fontsource/figtree/files/figtree-latin-ext-400-normal.woff';
+import faustina from '@fontsource/faustina/files/faustina-latin-400-normal.woff';
+import figtree from '@fontsource/figtree/files/figtree-latin-700-normal.woff';
 
 import { labelDrafts, sortByDate, filterOutDraftsIfProduction, getCategory, formatPostDate } from "../../utils/utils";
 
@@ -21,11 +22,11 @@ export async function getStaticPaths() {
 }
 
 const renderHtml = (content: CollectionEntry<'posts'> | CollectionEntry<'pages'>, category?: CollectionEntry<'categories'>) => {
-    
+
     const { title, description } = content.data;
     const date: Date | undefined = ('date' in content.data) ? content.data.date : undefined;
 
-    return html(`
+    return (`
         <style>
             ${styles}
         </style>
@@ -42,39 +43,45 @@ const renderHtml = (content: CollectionEntry<'posts'> | CollectionEntry<'pages'>
 }
 
 export const GET: APIRoute = async ({ params }) => {
-    const [ collectionRaw, slug ] = params.collectionAndSlug!.split(/\/(.*)/s);
+    const [collectionRaw, slug] = params.collectionAndSlug!.split(/\/(.*)/s);
     const collection = collectionRaw as ContentCollectionKey;
 
     const entry = await getEntryBySlug(collection, slug!!) as CollectionEntry<'posts'> | CollectionEntry<'pages'>;
     const category = (collection === 'posts') ? await getCategory(entry as CollectionEntry<'posts'>) : undefined;
-    
-    const svg = await satori(renderHtml(entry, category), {
+
+    const renderedHtml = renderHtml(entry, category);
+
+    const svg = await satori(htmlToSatori(renderedHtml), {
         width: 1200,
         height: 630,
         fonts: [
             {
-              name: 'Figtree',
-              data: figtree,
-              weight: 700,
-              style: 'normal',
+                name: 'Figtree',
+                data: figtree,
+                weight: 700,
+                style: 'normal',
             },
             {
-              name: 'Faustina',
-              data: faustina,
-              weight: 400,
-              style: 'normal',
+                name: 'Faustina',
+                data: faustina,
+                weight: 400,
+                style: 'normal',
             },
             {
-              name: 'Faustina',
-              data: faustina,
-              weight: 300,
-              style: 'normal',
+                name: 'Faustina',
+                data: faustina,
+                weight: 300,
+                style: 'normal',
             },
-          ],
-      });
+        ],
+    });
 
-    return new Response(
-        //TODO: generate png using data
-        svg
-    );
+    const responseData = await sharp(Buffer.from(svg)).png().toBuffer();
+    
+    return new Response(responseData, {
+        status: 200,
+        headers: {
+        "Content-Type": "image/png",
+        },
+    });
 }
