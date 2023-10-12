578c2f51ec7e2bab037805ce8bf473072fc5e421
Tyler Mercer <acct@tylermercer.net>
2023-10-11 20:09:04 -0600

    Refactor out image rendering code and use for posts and pages

--- src/pages/og-images/[...collectionAndSlug].png.ts
+++ src/pages/og-images/[...collectionAndSlug].png.ts
@@ -1,18 +1,8 @@
 import { getCollection, getEntryBySlug, type ContentCollectionKey, type CollectionEntry } from "astro:content";
 import type { APIRoute } from "astro";
-import { html as htmlToSatori } from "satori-html";
-import satori from "satori";
-import sharp from "sharp";
 
-import styles from './_styles.css?raw';
-import faustinaRaw from '@fontsource/faustina/files/faustina-latin-400-normal.woff';
-import figtreeRaw from '@fontsource/figtree/files/figtree-latin-700-normal.woff';
-
-// They already are Buffers because of the custom rawFonts Vite plugin in astro.config.js, but TS doesn't know that
-const figtree = figtreeRaw as unknown as Buffer;
-const faustina = faustinaRaw as unknown as Buffer;
-
-import { labelDrafts, sortByDate, filterOutDraftsIfProduction, getCategory, formatPostDate } from "../../utils/utils";
+import { labelDrafts, sortByDate, filterOutDraftsIfProduction, getCategory } from "../../utils/utils";
+import { renderImage } from "./_renderImage";
 
 export async function getStaticPaths() {
     const posts = labelDrafts(sortByDate(filterOutDraftsIfProduction(await getCollection("posts"))));
@@ -25,70 +15,14 @@ export async function getStaticPaths() {
         .concat(pages.map(toParams('pages')));
 }
 
-const renderHtml = (
-    content: CollectionEntry<'posts'> | CollectionEntry<'pages'>,
-    category?: CollectionEntry<'categories'>,
-    growTitleBox: boolean = false) => {
-
-    const { title, description } = content.data;
-    const date: Date | undefined = ('date' in content.data) ? content.data.date : undefined;
-
-    return (`
-        <style>
-            ${styles}
-        </style>
-        <div class="root">
-            <div class="pattern pattern-${category?.id || ''} ${growTitleBox ? 'grow' : ''}">
-                <h1 class="title">${title}</h1>
-            </div>
-            <div class="bg-container ${growTitleBox ? '' : 'grow'}">
-                <div class="description">${description}</div>
-                ${date ? `<div class="dateline">${formatPostDate(date)} — Tyler Mercer</div>` : ''}
-            </div>
-        </div>
-        `.trim());
-}
-
 export const GET: APIRoute = async ({ params }) => {
-    const [collectionRaw, slug] = params.collectionAndSlug!.split(/\/(.*)/s);
-    const collection = collectionRaw as ContentCollectionKey;
+    const [collection, slug] = params.collectionAndSlug!.split(/\/(.*)/s);
 
-    const entry = await getEntryBySlug(collection, slug!!) as CollectionEntry<'posts'> | CollectionEntry<'pages'>;
+    const entry = await getEntryBySlug(collection as unknown as ContentCollectionKey, slug!!) as CollectionEntry<'posts'> | CollectionEntry<'pages'>;
     const category = (collection === 'posts') ? await getCategory(entry as CollectionEntry<'posts'>) : undefined;
 
-    const renderedHtml = renderHtml(entry, category);
-
-    const svg = await satori(htmlToSatori(renderedHtml), {
-        width: 1200,
-        height: 630,
-        fonts: [
-            {
-                name: 'Figtree',
-                data: figtree,
-                weight: 700,
-                style: 'normal',
-            },
-            {
-                name: 'Faustina',
-                data: faustina,
-                weight: 400,
-                style: 'normal',
-            },
-            {
-                name: 'Faustina',
-                data: faustina,
-                weight: 300,
-                style: 'normal',
-            },
-        ],
-    });
-
-    const responseData = await sharp(Buffer.from(svg)).png().toBuffer();
-    
-    return new Response(responseData, {
-        status: 200,
-        headers: {
-        "Content-Type": "image/png",
-        },
+    return await renderImage({
+        ...entry.data,
+        category: category?.id
     });
 }
---
+++ src/pages/og-images/_renderImage.ts
@@ -0,0 +1,83 @@
+import { html as htmlToSatori } from "satori-html";
+import satori from "satori";
+import sharp from "sharp";
+
+import styles from './_styles.css?raw';
+import faustinaRaw from '@fontsource/faustina/files/faustina-latin-400-normal.woff';
+import figtreeRaw from '@fontsource/figtree/files/figtree-latin-700-normal.woff';
+import { formatPostDate } from "../../utils/utils";
+
+// They already are Buffers because of the custom rawFonts Vite plugin in astro.config.js, but TS doesn't know that
+const figtree = figtreeRaw as unknown as Buffer;
+const faustina = faustinaRaw as unknown as Buffer;
+
+type Options = {
+    title: string,
+    description: string,
+    category?: string,
+    growTitleBox?: boolean,
+    date?: Date
+}
+
+const getHtml = (
+    options: Options) => {
+
+    const { title, description, growTitleBox, category } = options;
+    const date: Date | undefined = ('date' in options) ? options.date : undefined;
+
+    return (`
+        <style>
+            ${styles}
+        </style>
+        <div class="root">
+            <div class="pattern pattern-${category || ''} ${growTitleBox ? 'grow' : ''}">
+                <h1 class="title">${title}</h1>
+            </div>
+            <div class="bg-container ${growTitleBox ? '' : 'grow'}">
+                <div class="description">${description}</div>
+                ${date ? `<div class="dateline">${formatPostDate(date)} — Tyler Mercer</div>` : ''}
+            </div>
+        </div>
+        `.trim());
+}
+
+const htmlToImage = async (renderedHtml: string) => {
+    const svg = await satori(htmlToSatori(renderedHtml), {
+        width: 1200,
+        height: 630,
+        fonts: [
+            {
+                name: 'Figtree',
+                data: figtree,
+                weight: 700,
+                style: 'normal',
+            },
+            {
+                name: 'Faustina',
+                data: faustina,
+                weight: 400,
+                style: 'normal',
+            },
+            {
+                name: 'Faustina',
+                data: faustina,
+                weight: 300,
+                style: 'normal',
+            },
+        ],
+    });
+
+    return await sharp(Buffer.from(svg)).png().toBuffer();
+}
+
+const imageToResponse = (image: Buffer) =>
+    new Response(image, {
+        status: 200,
+        headers: {
+            "Content-Type": "image/png",
+        },
+    });
+
+export async function renderImage(options: Options) {
+    return imageToResponse(await htmlToImage(getHtml(options)));
+}
