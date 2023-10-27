df890e7ba3b5bc27d0add98989938be1cef31e6d
Tyler Mercer <acct@tylermercer.net>
2023-10-26 19:23:02 -0600

    Make SG api call from worker

--- functions/api/submit-form.js
+++ functions/api/submit-form.js
@@ -1,3 +1,34 @@
-export function onRequestGet(context) {
-    return new Response("Hello, world!")
-  }
+export async function onRequestGet(context) {
+  const sendgridApiKey = 'YOUR_API_KEY_HERE'; // Replace with your SendGrid API key
+  const url = 'https://api.sendgrid.com/v3/marketing/contacts';
+
+  const payload = {
+    contacts: [
+      // {
+      //   email: 'foobar@example.com',
+      //   custom_fields: { w1: 'coffee', w33: '42', e2: 'blue' }
+      // }
+    ]
+  };
+
+  const headers = {
+    'Authorization': `Bearer ${sendgridApiKey}`,
+    'Content-Type': 'application/json',
+  };
+
+  const fetchOptions = {
+    method: 'PUT',
+    headers,
+    body: JSON.stringify(payload),
+  };
+
+  // Send the Fetch request to SendGrid
+  const response = await fetch(url, fetchOptions).then(r => r.text());
+
+  // Return the response from SendGrid
+  return new Response(response, {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+}
