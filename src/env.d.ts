c44387b11cb4430344bc1e4f0818abed4e5d9f25
Tyler Mercer <acct@tylermercer.net>
2024-08-31 22:51:28 -0600

    Move form submit handler into Astro endpoint

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,15 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+type Runtime = import('@astrojs/cloudflare').Runtime<Env>;
+
+declare namespace App {
+    interface Locals extends Runtime {
+        env: Env
+    }
+    interface Env {
+        SENDGRID_API_KEY: string;
+        SENDGRID_LIST_ID: string;
+        SENDGRID_WELCOME_TEMPLATE_ID: string;
+    }
+}
