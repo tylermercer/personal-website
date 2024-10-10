8b7fcbf778fceb95c1cd43d0b2f2e1a6c765e302
Tyler Mercer <acct@tylermercer.net>
2024-10-10 06:48:43 -0600

    nomerge! [Get working] Add fontaine
    Builds, but fallback does not show in site output

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,29 @@ import mdx from "@astrojs/mdx";
 import expressiveCode from "astro-expressive-code";
 import { defineConfig } from 'astro/config';
 import remarkEmdash from './src/plugins/remark/emdash';
+import { FontaineTransform } from 'fontaine';
+
+const fontaine = FontaineTransform.vite({
+  fallbacks: ['BlinkMacSystemFont', 'Segoe UI', 'Helvetica Neue', 'Arial', 'Noto Sans'],
+});
+
+function rawFontsOrFontaine(ext) {
+  return {
+    name: 'vite-plugin-raw-fonts',
+    transform(code, id) {
+      if (ext.some(e => id.endsWith(e))) {
+        const buffer = fs.readFileSync(id);
+        return {
+          code: `export default Buffer.from(${JSON.stringify(buffer)})`,
+          map: null
+        };
+      }
+      else if (!id.includes('?raw')) {
+        return fontaine.transform(code, id);
+      }
+    },
+  };
+}
 
 // https://astro.build/config
 export default defineConfig({
@@ -22,7 +45,9 @@ export default defineConfig({
     server: {
       hostname: 'tylermercer.localhost'
     },
-    plugins: [rawFonts(['.woff'])],
+    plugins: [
+      rawFontsOrFontaine(['.woff']),
+    ],
     ssr: {
       external: [
         'markdown-it',
@@ -31,17 +56,3 @@ export default defineConfig({
     }
   },
 });
-function rawFonts(ext) {
-  return {
-    name: 'vite-plugin-raw-fonts',
-    transform(_, id) {
-      if (ext.some(e => id.endsWith(e))) {
-        const buffer = fs.readFileSync(id);
-        return {
-          code: `export default Buffer.from(${JSON.stringify(buffer)})`,
-          map: null
-        };
-      }
-    },
-  };
-}
