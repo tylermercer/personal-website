282f3d56b4cbf47fd76530e913abce1d821b7b62
Tyler Mercer <acct@tylermercer.net>
2024-10-18 07:32:33 -0600

    Fix bug where innerhelm feed drafts had Jan 1 1970 as date

--- src/utils/getPostDate.ts
+++ src/utils/getPostDate.ts
@@ -1,10 +1,8 @@
 import { type CollectionEntry } from "astro:content";
 
-const now = new Date();
-
 export default function getPostDate(entry: CollectionEntry<'posts'>) {
     if (entry.data.date) {
         return entry.data.date;
     }
-    else return now;
+    else return new Date(); //must be function-scoped to get current date on CF Workers
 }
