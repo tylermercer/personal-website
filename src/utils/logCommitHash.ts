f4ecda1a46514c06c06ca49daa07deb270fafb2e
Tyler Mercer <acct@tylermercer.net>
2024-11-04 21:28:38 -0700

    Only log first eight characters of commit hash

--- src/utils/logCommitHash.ts
+++ src/utils/logCommitHash.ts
@@ -1,3 +1,3 @@
 export default function logCommitHash() {
-    console.log(`Version: ${import.meta.env.PUBLIC_COMMIT_HASH}`)
+    console.log(`Version: ${(import.meta.env.PUBLIC_COMMIT_HASH).substring(0, 8)}`)
 }
