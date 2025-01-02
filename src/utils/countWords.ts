b66b46dbec37dd095d69d32bf0ea2ec090f94544
Tyler Mercer <40773575+tylermercer@users.noreply.github.com>
2025-01-02 10:04:49 -0700

    Only count words before footnotes

--- src/utils/countWords.ts
+++ src/utils/countWords.ts
@@ -1,6 +1,6 @@
 // Adapted from Nunjuck's wordcount filter implementation
 
 export default function countWords(str: string): number {
-    const words = (str) ? str.match(/\w+/g) : null;
-    return (words) ? words.length : 0;
+    const words = str?.split(`\n[^`).at(0)?.match(/\w+/g);
+    return words?.length ?? 0;
 }
