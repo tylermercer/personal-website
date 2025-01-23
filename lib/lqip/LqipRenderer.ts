d44c3d59f3675b4a2965ad2135d7d68ae9cb8eff
Tyler Mercer <acct@tylermercer.net>
2025-01-22 20:01:52 -0700

    Fix LqipRenderer in lib

--- lib/lqip/LqipRenderer.ts
+++ lib/lqip/LqipRenderer.ts
@@ -6,7 +6,7 @@ export default class LqipRenderer {
 
     private async assetToBase64Webp(file: string) {
 
-        const base64Image = (await cache.getOrCreate(await generateCacheKey({ file }), async () => {
+        const base64Image = (await this.cache.getOrCreate(await generateCacheKey({ file }), async () => {
             return Buffer.from((await sharp(file)
                 .resize(16)
                 .webp()
@@ -19,24 +19,24 @@ export default class LqipRenderer {
     
     public async getLqipStyles(imagePath: string) {
         const webpImage = await this.assetToBase64Webp(imagePath);
-    return `
-        color: transparent;
-        background-size: cover;
-        background-position: center;
-        background-repeat: no-repeat;
-        background-image: url('data:image/svg+xml;charset=utf-8,\
-            <svg xmlns="http://www.w3.org/2000/svg">\
-                <filter id="b" color-interpolation-filters="sRGB">\
-                    <feGaussianBlur stdDeviation="20"/>\
-                    <feComponentTransfer>\
-                        <feFuncA type="discrete" tableValues="1 1"/>\
-                    </feComponentTransfer>\
-                </filter>\
-                <g filter="url(%23b)">\
-                    <image width="100%" height="100%" preserveAspectRatio="xMidYMid slice" href="${webpImage}"/>\
-                </g>\
-            </svg>');
-    `;
-    }
+        return `
+            color: transparent;
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-image: url('data:image/svg+xml;charset=utf-8,\
+                <svg xmlns="http://www.w3.org/2000/svg">\
+                    <filter id="b" color-interpolation-filters="sRGB">\
+                        <feGaussianBlur stdDeviation="20"/>\
+                        <feComponentTransfer>\
+                            <feFuncA type="discrete" tableValues="1 1"/>\
+                        </feComponentTransfer>\
+                    </filter>\
+                    <g filter="url(%23b)">\
+                        <image width="100%" height="100%" preserveAspectRatio="xMidYMid slice" href="${webpImage}"/>\
+                    </g>\
+                </svg>');
+        `;
+    }   
 }
 
