8ef68d200869a8daa9df7f8a05a5884ec30e291c
Tyler Mercer <acct@tylermercer.net>
2025-03-04 22:09:18 -0700

    Fix internal links in RSS

--- src/utils/renderMarkdown.ts
+++ src/utils/renderMarkdown.ts
@@ -1,15 +1,23 @@
 
-import { remark } from 'remark';
-import remarkRehype from 'remark-rehype';
-import rehypeStringify from 'rehype-stringify';
 import remarkEmdash from 'lib/plugins/remark/emdash';
+import rehypeRewrite from 'rehype-rewrite';
+import rehypeStringify from 'rehype-stringify';
+import { remark } from 'remark';
 import remarkGfm from 'remark-gfm';
+import remarkRehype from 'remark-rehype';
 
 
 const md = remark()
     .use(remarkGfm)
     .use(remarkEmdash as any)
     .use(remarkRehype)
+    .use(rehypeRewrite, {
+      rewrite: (node) => {
+        if(node.type == 'element' && node.tagName == 'a' && node.properties.href!.toString().startsWith('/')) {
+          node.properties.href = `https://tylermercer.net${node.properties.href}`;
+        }
+      }
+    })
     .use(rehypeStringify);
 
 export default async function renderMarkdown(markdown: string): Promise<string> {
